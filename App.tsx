421c1812ec3b3b5059768cb1d222a1eae288b81b
Felipe Bis <felipe.bis@aluno.ifsp.edu.br>
2023-08-23 21:59:19 -0300

    feat: setup navigation router

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Routes from './src/routes';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App: React.FC = () => {
+    return (
+        <SafeAreaProvider style={styles.container}>
+            <Routes />
+        </SafeAreaProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+    },
 });
+
+export default App;
---
+++ src/routes/index.tsx
@@ -0,0 +1,41 @@
+import React, { useMemo } from 'react';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { Login } from '../pages';
+import { RootParamList } from '../utils/types';
+
+const Stack = createNativeStackNavigator<RootParamList>();
+
+
+const Routes: React.FC = () => {
+    const isLoggedIn = false;
+
+    const newRoutes = useMemo(
+        () =>
+            isLoggedIn ?
+                (
+                    <Stack.Screen name="Login" component={Login} />
+                )
+                :
+                (
+                    <Stack.Screen name="Login" component={Login} />
+                ), [])
+
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false
+                }}
+                initialRouteName="Login"
+            >
+                {newRoutes}
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default Routes;
